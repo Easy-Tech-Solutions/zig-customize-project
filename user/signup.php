e882287ecfa44c36e7ea6d9f47c0f72065f2ece5
Rabidalton <rabiakalimat02@gmail.com>
2025-05-09 14:37:12 +0000

    modified registration

--- user/signup.php
+++ user/signup.php
@@ -18,25 +18,25 @@ if(isset($_POST['userreg'])){
     
     // Check if Terms & Conditions checkbox is checked
     if(!isset($_POST['accept'])) {
-        header("Location: /pages/registration.php?error=terms");
+        header("Location: ../pages/registration.php?error=terms");
         exit();
     }
 
     // Validate email
     if (!$mail) {
-        header("Location: /pages/registration.php?error=email");
+        header("Location: ../pages/registration.php?error=email");
         exit();
     }
 
     // Password requirements
     if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/', $password)) {
-        header("Location: /pages/registration.php?error=password");
+        header("Location: ../pages/registration.php?error=password");
         exit();
     }
 
     // Check if passwords match
     if ($password !== $confirmPassword) {
-        header("Location: /pages/registration.php?error=password_match");
+        header("Location: ../pages/registration.php?error=password_match");
         exit();
     }
 
@@ -50,7 +50,7 @@ if(isset($_POST['userreg'])){
         $stmt->execute([$username, $mail]);
         
         if($stmt->fetchColumn() > 0) {
-            header("Location: /pages/registration.php?error=exists");
+            header("Location: ../pages/registration.php?error=exists");
             exit();
         }
 
@@ -63,7 +63,7 @@ if(isset($_POST['userreg'])){
         // Start session and log user in automatically if desired
         session_start();
         $_SESSION['registration_success'] = true;
-        header("Location: /pages/login.php");
+        header("Location: ../pages/login.php");
         exit();
 
     } catch (PDOException $e) {
@@ -71,7 +71,7 @@ if(isset($_POST['userreg'])){
         error_log("Registration error: " . $e->getMessage());
         
         // Redirect with generic error
-        header("Location: /pages/registration.php?error=database");
+        header("Location: ../pages/registration.php?error=database");
         exit();
     }
 }
