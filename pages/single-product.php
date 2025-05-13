e13a6c6cee0e3507ad64c5453f7ffc6fdfe109a4
Rabidalton <rabiakalimat02@gmail.com>
2025-05-13 16:45:19 +0000

    modified singleproduct backend code

--- pages/single-product.php
+++ pages/single-product.php
@@ -88,10 +88,71 @@ foreach ($reviews as $review) {
 }
 ?>
 
-<div class="page-detail u-s-p-t-80">
-    <div class="container">
+<!DOCTYPE html>
+<html class="no-js" lang="en-US">
+
+	<head>
+    <meta charset="UTF-8">
+    <!--[if IE]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>ZIG CUSTOMIZED</title>
+    <!-- Standard Favicon -->
+    <link href="../assets/images/favicon/favicon.ico" rel="shortcut icon">
+    <!-- Base Google Font for Web-app -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <!-- Google Fonts for Banners only -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet">
+    <!-- Bootstrap 4 -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <!-- Font Awesome 5 -->
+    <link rel="stylesheet" href="../assets/css/fontawesome.min.css">
+    <!-- Ion-Icons 4 -->
+    <link rel="stylesheet" href="../assets/css/ionicons.min.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="../assets/css/animate.min.css">
+    <!-- Owl-Carousel -->
+    <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+    <!-- Jquery-Ui-Range-Slider -->
+    <link rel="stylesheet" href="../assets/css/jquery-ui-range-slider.min.css">
+    <!-- Utility -->
+    <link rel="stylesheet" href="../assets/css/utility.css">
+    <!-- Main -->
+    <link rel="stylesheet" href="../assets/css/bundle.css">
+</head>
+
+	<body>
+
+<!-- app -->
+	<div id="app">
+    <!-- Header -->
+    <?php include("../include/header.php"); ?>
+    <!-- Header /- -->
+    <!-- Page Introduction Wrapper -->
+    <div class="page-style-a">
+        <div class="container">
+            <div class="page-intro">
+                <h2>Detail</h2>
+                <ul class="bread-crumb">
+                    <li class="has-separator">
+                        <i class="ion ion-md-home"></i>
+                        <a href="../index.html">Home</a>
+                    </li>
+                    <li class="is-marked">
+                        <a href="./single-product.php">Detail</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+	
+	<div class="page-detail u-s-p-t-80">
+		<div class="container">
         <!-- Product-Detail -->
-        <div class="row">
+			<div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12">
                 <!-- Product-zoom-area -->
                 <div class="zoom-area">
@@ -441,36 +502,116 @@ foreach ($reviews as $review) {
         </div>
         <!-- Detail-Tabs /- -->
     </div>
+	</div>
+	
+    <div class="select-dummy-wrapper">
+        <select id="compute-select">
+            <option id="compute-option">All</option>
+        </select>
+    </div>
+    <!-- Dummy Selectbox /- -->
+    <!-- Responsive-Search -->
+    <div class="responsive-search-wrapper">
+        <button type="button" class="button ion ion-md-close" id="responsive-search-close-button"></button>
+        <div class="responsive-search-container">
+            <div class="container">
+                <p>Start typing and press Enter to search</p>
+                <form class="responsive-search-form">
+                    <label class="sr-only" for="search-text">Search</label>
+                    <input id="search-text" type="text" class="responsive-search-field" placeholder="PLEASE SEARCH">
+                    <i class="fas fa-search"></i>
+                </form>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
-<script>
-// JavaScript for star rating input
-document.addEventListener('DOMContentLoaded', function() {
-    const starContainer = document.querySelector('.star-wrapper .star');
-    const ratingInput = document.getElementById('your-rating-value');
-    const starComment = document.getElementById('star-comment');
-    
-    const comments = [
-        "Very Poor",
-        "Poor",
-        "Average",
-        "Good",
-        "Excellent"
-    ];
-    
-    starContainer.addEventListener('click', function(e) {
-        const rect = starContainer.getBoundingClientRect();
-        const pos = (e.clientX - rect.left) / rect.width;
-        const rating = Math.min(5, Math.max(1, Math.ceil(pos * 5)));
-        
-        ratingInput.value = rating;
-        starContainer.querySelector('span').style.width = (rating / 5) * 100 + 'px';
-        starComment.textContent = comments[rating - 1];
-    });
-    
-    // Sort reviews when dropdown changes
-    document.getElementById('review-sort').addEventListener('change', function() {
-        // You can implement AJAX sorting here if needed
-    });
-});
-</script>
+	<noscript>
+    <div class="app-issue">
+        <div class="vertical-center">
+            <div class="text-center">
+                <h1>JavaScript is disabled in your browser.</h1>
+                <span>Please enable JavaScript in your browser or upgrade to a JavaScript-capable browser to register for Groover.</span>
+            </div>
+        </div>
+    </div>
+    <style>
+    #app {
+        display: none;
+    }
+    </style>
+</noscript>
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+
+	<script>
+	// JavaScript for star rating input
+	document.addEventListener('DOMContentLoaded', function() {
+		const starContainer = document.querySelector('.star-wrapper .star');
+		const ratingInput = document.getElementById('your-rating-value');
+		const starComment = document.getElementById('star-comment');
+		
+		const comments = [
+			"Very Poor",
+			"Poor",
+			"Average",
+			"Good",
+			"Excellent"
+		];
+		
+		starContainer.addEventListener('click', function(e) {
+			const rect = starContainer.getBoundingClientRect();
+			const pos = (e.clientX - rect.left) / rect.width;
+			const rating = Math.min(5, Math.max(1, Math.ceil(pos * 5)));
+			
+			ratingInput.value = rating;
+			starContainer.querySelector('span').style.width = (rating / 5) * 100 + 'px';
+			starComment.textContent = comments[rating - 1];
+		});
+		
+		// Sort reviews when dropdown changes
+		document.getElementById('review-sort').addEventListener('change', function() {
+			// You can implement AJAX sorting here if needed
+		});
+	});
+
+	window.ga = function() {
+		ga.q.push(arguments)
+	};
+	ga.q = [];
+	ga.l = +new Date;
+	ga('create', 'UA-XXXXX-Y', 'auto');
+	ga('send', 'pageview')
+	</script>
+
+	<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+	<!-- Modernizr-JS -->
+	<script type="text/javascript" src="../assets/js/vendor/modernizr-custom.min.js"></script>
+	<!-- NProgress -->
+	<script type="text/javascript" src="../assets/js/nprogress.min.js"></script>
+	<!-- jQuery -->
+	<script type="text/javascript" src="../assets/js/jquery.min.js"></script>
+	<!-- Bootstrap JS -->
+	<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+	<!-- Popper -->
+	<script type="text/javascript" src="../assets/js/popper.min.js"></script>
+	<!-- ScrollUp -->
+	<script type="text/javascript" src="../assets/js/jquery.scrollUp.min.js"></script>
+	<!-- Elevate Zoom -->
+	<script type="text/javascript" src="../assets/js/jquery.elevatezoom.min.js"></script>
+	<!-- jquery-ui-range-slider -->
+	<script type="text/javascript" src="../assets/js/jquery-ui.range-slider.min.js"></script>
+	<!-- jQuery Slim-Scroll -->
+	<script type="text/javascript" src="../assets/js/jquery.slimscroll.min.js"></script>
+	<!-- jQuery Resize-Select -->
+	<script type="text/javascript" src="../assets/js/jquery.resize-select.min.js"></script>
+	<!-- jQuery Custom Mega Menu -->
+	<script type="text/javascript" src="../assets/js/jquery.custom-megamenu.min.js"></script>
+	<!-- jQuery Countdown -->
+	<script type="text/javascript" src="../assets/js/jquery.custom-countdown.min.js"></script>
+	<!-- Owl Carousel -->
+	<script type="text/javascript" src="../assets/js/owl.carousel.min.js"></script>
+	<!-- Main -->
+	<script type="text/javascript" src="../assets/js/app.js"></script>
+</body>
+</html>
