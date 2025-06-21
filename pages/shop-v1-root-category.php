b167f5c60c52c9308173e7f0584d6cee640e39d1
Rabidalton <rabiakalimat02@gmail.com>
2025-06-21 23:26:11 +0000

    changes made

--- pages/shop-v1-root-category.php
+++ pages/shop-v1-root-category.php
@@ -232,7 +232,7 @@ require_once('../sql_connection/config.php');
                             $price = number_format($product['price'], 2);
                             $originalPrice = $product['original_price'] ? number_format($product['original_price'], 2) : null;
                         ?>
-                            <div class="product-item col-lg-4 col-md-6 col-sm-6">
+                            <div class="product-item col-6 col-lg-3 mb-3">
                                 <div class="item">
                                     <div class="image-container">
                                         <a class="item-img-wrapper-link" href="./single-product.php?id=<?= $product['id'] ?>">
