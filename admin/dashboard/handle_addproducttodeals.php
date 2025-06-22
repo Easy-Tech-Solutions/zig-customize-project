d02a237e98c004f7550012a57c649f65f9612c98
Rabidalton <rabiakalimat02@gmail.com>
2025-06-22 13:21:08 +0000

    changes made

--- admin/dashboard/handle_addproducttodeals.php
+++ admin/dashboard/handle_addproducttodeals.php
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['flash_status'] = 'danger';
     }
     
-    header('Location: add-special-product.php');
+    header('Location: addproducttodeals.php');
     exit();
 }
 ?>
